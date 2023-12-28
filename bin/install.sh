1003156cb469c81543fd7a3c349642a3b421cf1d
BrandoV1 <152193480+BrandoV1@users.noreply.github.com>
2023-12-28 13:38:07 +0100

    install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -133,10 +133,10 @@ fi
 if [ -z "${REPOSITORY}" ]; then
   if [ "$WEB_UPGRADE" = false ]; then
     set -x
-    REPOSITORY=${1:-https://github.com/BrandoV1/Narrowcast}
+    REPOSITORY=${1:-https://github.com/BrandoV1/Narrowcast/tree/1729/.github}
   else
     set -e
-    REPOSITORY=https://github.com/BrandoV1/Narrowcast
+    REPOSITORY=https://github.com/BrandoV1/Narrowcast/tree/1729/.github
   fi
 fi
 
